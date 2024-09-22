3dd44e761fd00873e93ad845b89eeca34391a9c8
Brainwit <brainwit1@gmail.com>
2024-09-22 15:10:03 -0500

    cambio-tiempo

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,18 +4,43 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-let countDown = new Date('Sep 21, 2024 18:00:00').getTime(),
+let countDown = new Date('Sep 21, 2024 18:00:10').getTime(),
   x = setInterval(function () {
 
     let now = new Date().getTime(),
       distance = countDown - now;
 
-    document.getElementById('dias').innerText = Math.floor(distance / (day)),
-      document.getElementById('horas').innerText = Math.floor((distance % (day)) / (hour)),
-      document.getElementById('minutos').innerText = Math.floor((distance % (hour)) / (minute)),
-      document.getElementById('segundos').innerText = Math.floor((distance % (minute)) / second);
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById('dias').innerText = 0;
+      document.getElementById('horas').innerText = 0;
+      document.getElementById('minutos').innerText = 0;
+      document.getElementById('segundos').innerText = 0;
+      console.log("¡El tiempo ha terminado!");
+      return; // Termina la ejecución de la función
+    }
+
+    const dias = Math.floor(distance / day);
+    const horas = Math.floor((distance % day) / hour);
+    const minutos = Math.floor((distance % hour) / minute);
+    const segundos = Math.floor((distance % minute) / second);
+
+    document.getElementById('dias').innerText = dias;
+    document.getElementById('horas').innerText = horas;
+    document.getElementById('minutos').innerText = minutos;
+    document.getElementById('segundos').innerText = segundos;
+
+    console.log(dias, horas, minutos, segundos);
 
   }, second);
+
+
+if (segundos == 0) {
+  document.getElementById('dias').innerText = "00",
+    document.getElementById('horas').innerText = "00",
+    document.getElementById('minutos').innerText = "00",
+    document.getElementById('segundos').innerText = "00";
+}
 
 
 /* music */
